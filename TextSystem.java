9864bd9f40e2660749f2ac39eec9900d656165a3
ningwumu <41458257+ningwumu@users.noreply.github.com>
2018-07-20 15:39:33 +0800

    Add files via upload

---
+++ TextSystem.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class TextSystem {
+public static void main(String[] args) {
+	do{
+	System.out.println("**************************");
+	System.out.println("1,I want to booking");
+	System.out.println("2,check");
+	System.out.println("3,receipt order form");
+	System.out.println("4,delete order form");
+	System.out.println("5,like");
+	System.out.println("6,out of system");
+	System.out.println("***************************");
+	System.out.println("please choose");
+	Scanner input=new Scanner(System.in);
+	int choose=input.nextInt();
+	switch (choose) {
+	case 1:
+		
+		break;
+	case 2:
+		
+		break;
+	case 3:
+		
+		break;
+	case 4:
+		
+		break;
+	case 5:
+		
+		break;
+	case 6:
+		
+		break;
+	}
+	
+	}while(choose==0);
+	}
+}
